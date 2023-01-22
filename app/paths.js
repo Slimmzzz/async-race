695873b5436597a04758d7e08c174b2a48334324
Max Khotsin <slimgos22@gmail.com>
2023-01-22 22:23:51 +0300

    docs: attach bundle

--- app/paths.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  // Source files
-  app: path.resolve(__dirname, './app'),
-
-  // Production build files
-  build: path.resolve(__dirname, './dist'),
-}
